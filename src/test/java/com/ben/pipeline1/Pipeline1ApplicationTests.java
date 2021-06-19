588d4ce229c883e7917a741004d6e3b9b91232e4
denibenji89 <benjichan89@hotmail.com>
2021-06-19 09:56:33 -0700

    first commit

---
+++ src/test/java/com/ben/pipeline1/Pipeline1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben.pipeline1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pipeline1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
